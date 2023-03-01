d21249dfb31af4e233b6aae8b93d74a8e5904718
sahil110802 <97180986+sahil110802@users.noreply.github.com>
2023-03-01 19:26:13 +0530

    Add files via upload

---
+++ src/components/Note.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+export default Note;
+
+
+
+
+
+// //https://dotmovies.us/download-girls-hostel-season-3-hindi-sonyliv-complete-series-480p-720p-1080p/
+
+
+
+// https://vegamovies.day/download-the-hills-have-eyes-2-2007-english-with-subtitles-480p-720p/
+
+
+
+// https://vegamovies.day/download-immature-2022-season-2-hindi-complete-amazon-prime-video-web-series-480p-720p-1080p/
